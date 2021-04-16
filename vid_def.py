03ff03c75ac692b933d9bb859eac9eddcb433c8f
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-04-16 17:04:07 -0400

    Add dataclasses for video definitions

---
+++ vid_def.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Segment:
+
+    # The name of the item in this segment.
+    name: str
+    # The URL of the image to display in this segment.
+    image_url: str
+
+
+@dataclass
+class VideoDef:
+
+    # The title of the video.
+    title: str
+    # The YouTube description of the video.
+    description: str
+    # A list of segments to include in the video.
+    segments: List[Segment]
+
+
+def video_def_from_list_url(url: str) -> VideoDef:
+    return None
