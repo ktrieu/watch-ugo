4db4cf70c878405c47caf7a1158f8755bcc5a9fb
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-04-17 15:20:50 -0400

    Add some graphical overlays as well as text to go with it

---
+++ render.py
@@ -0,0 +1,7 @@
+# We're aiming for 1080p on YouTube
+VIDEO_WIDTH = 1920
+VIDEO_HEIGHT = 1080
+
+# Landmarks for text insertion, in (top left), (top right) format
+NUMBER_TEXT_BOX = ((122, 95), (248, 221))
+SEGMENT_NAME_BOX = ((113, 875), (912, 986))
